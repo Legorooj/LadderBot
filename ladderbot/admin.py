82cc5b2f68917bf661029317a421a97a30854658
Legorooj <legorooj@protonmail.com>
2021-02-04 11:20:25 +1100

    and a bit more

--- ladderbot/admin.py
+++ ladderbot/admin.py
@@ -243,7 +243,7 @@ class Admin(commands.Cog):
         for player in db.Player.query():
             player: db.Player
             
-            games = player.complete().filter(db.Game.is_confirmed.is_(True)).order_by(db.Game.win_claimed_ts.asc())
+            games = player.complete().filter(db.Game.is_confirmed.is_(True)).order_by(db.Game.win_claimed_ts.desc())
             
             player_rung = 1
             

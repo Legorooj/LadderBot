976e87e798c64b3e2ca1c4afdbcfd06530cb07fe
Legorooj <legorooj@protonmail.com>
2021-02-07 15:46:41 +1100

    add backup script

---
+++ backup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+DAY=$(date +%A)
+TARGET=/home/legorooj/backups/polyladder_bak-${DAY}.sqlc
+FULLTARGET=/home/legorooj/backups/polyladder_full_backup.sqlc
+
+/usr/bin/pg_dump -U postgres -Fc  polyladder > $TARGET
+/usr/bin/pg_dump -U postgres -Fc polyladder > $FULLTARGET
+
+if [ $? -eq 0 ]
+then
+  echo "Backup successful to file $TARGET"
+  exit 0
+else
+  echo "Error during pg_dump" >&2
+  exit 1
+fi
